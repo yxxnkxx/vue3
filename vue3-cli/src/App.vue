bb89066f7e8ce0e76408e88af9ec8a5c57baae5c
YunGyung Lee <yxxnkxx@gmail.com>
2024-01-13 21:55:19 +0900

    vue3 cli

---
+++ vue3-cli/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <AppHeader :appTitle="message" v-on:change="changeMessage"></AppHeader>
+  <div>
+    {{ message }}
+  </div>
+  <button @click="showAlert">경고</button>
+</template>
+
+<script>
+// import 컴포넌트이름 from '경로'
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  components: {
+    AppHeader,
+  },
+  data() {
+    return {
+      message: '앱 헤더 컴포넌트',
+    };
+  },
+  methods: {
+    showAlert() {
+      alert('hi');
+    },
+    changeMessage() {
+      this.message = '변경됨';
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ vue3-form/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <form action="" @submit.prevent="submitForm">
+    <div>
+      <label for="userId">아이디</label>
+      <input id="userId" type="text" v-model="username" />
+    </div>
+    <div>
+      <label for="password">PW</label>
+      <input id="password" type="text" v-model="password" />
+    </div>
+    <button type="submit">로그인</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+import { ref } from 'vue';
+
+export default {
+  // data
+  setup() {
+    var username = ref('');
+    var password = ref('');
+
+    // methods
+    var submitForm = () => {
+      axios
+        .post('https://jsonplaceholder.typicode.com/users', {
+          username: username.value,
+          password: password.value,
+        })
+        .then((response) => {
+          console.log(response);
+        });
+    };
+    return { username, password, submitForm };
+  },
+
+  // data() {
+  //   return {
+  //     username: '',
+  //     password: '',
+  //   };
+  // },
+  // methods: {
+  //   submitForm() {
+  //     const data = {
+  //       username: this.username,
+  //       password: this.password,
+  //     };
+  //     axios.post('https://jsonplaceholder.typicode.com/users', data).then((response) => {
+  //       console.log(response);
+  //     });
+  //     console.log('제출됨');
+  //   },
+  // },
+};
+</script>
+
+<style scoped></style>
